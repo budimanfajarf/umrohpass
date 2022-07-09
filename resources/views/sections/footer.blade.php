ef47f2188cca946876696ededeef2d219f33f765
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2022-07-10 02:01:51 +0700

    Add primary & footer navigation

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -1,3 +1,8 @@
 <footer class="content-info">
-  @php(dynamic_sidebar('sidebar-footer'))
+  {{-- @php(dynamic_sidebar('sidebar-footer')) --}}
+  @if (has_nav_menu('footer_navigation'))
+    <nav class="nav-footer" aria-label="{{ wp_get_nav_menu_name('footer_navigation') }}">
+      {!! wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
+    </nav>
+  @endif
 </footer>
