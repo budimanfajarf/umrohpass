5929fce8996703a1084001f6ff3c95953bf40d1d
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2022-07-10 15:06:34 +0700

    Fix ignore

--- public/vendor/app.js
+++ public/vendor/app.js
@@ -1,6 +1,6 @@
 (self["webpackChunksage"] = self["webpackChunksage"] || []).push([["vendor/app"],{
 
-/***/ "./node_modules/slick-carousel/slick/slick.js":
+/***/ 154:
 /***/ (function(module, exports, __webpack_require__) {
 
 var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*
