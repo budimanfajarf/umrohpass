67ebe4300b7a4adaf1f7b131d84d30300eb2cde6
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2022-07-16 02:10:25 +0700

    Fix screens & container setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,30 @@
 module.exports = {
   content: ['./index.php', './app/**/*.php', './resources/**/*.{php,vue,js}'],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
     container: {
       center: true,
       padding: {
         DEFAULT: '1.25rem',
         sm: '2rem',
-        lg: '2.5rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6.25rem',
       },
     },
     extend: {
