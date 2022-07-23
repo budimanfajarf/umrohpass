b442599503b9b0787ba46536c63c6cab63a7913b
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2022-07-24 01:42:28 +0700

    Refactor header & mobile menu to improve toggle experience

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,15 +4,15 @@
 
 @include('sections.header')
 
-  <main id="main" class="main">
-    @yield('content')
-  </main>
+<main id="main" class="main">
+  @yield('content')
+</main>
 
-  @hasSection('sidebar')
-    <aside class="sidebar">
-      @yield('sidebar')
-    </aside>
-  @endif
+@hasSection('sidebar')
+  <aside class="sidebar">
+    @yield('sidebar')
+  </aside>
+@endif
 
 @include('sections.footer')
 
